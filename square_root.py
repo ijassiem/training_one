de262ef2234ec6f195eec01ddbbe58aec98076ac
ijassiem <ijassiem@ska.ac.za>
2021-06-01 14:16:05 +0200

    Added square root

---
+++ square_root.py
@@ -0,0 +1,11 @@
+"""
+This is an example docstring.
+
+A docstring with a summary line, a blank line
+and some more text like here.
+"""
+
+
+def square(n):
+    """Take square root of number."""
+    return n ** 0.5
