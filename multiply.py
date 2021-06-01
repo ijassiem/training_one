4118ccf8d35c472b625cc33cf9052a24e25d1348
ijassiem <ijassiem@ska.ac.za>
2021-06-01 14:48:24 +0200

    Added mod,divide,multiply

---
+++ multiply.py
@@ -0,0 +1,11 @@
+"""
+This is an example docstring.
+
+A docstring with a summary line, a blank line
+and some more text like here.
+"""
+
+
+def multiply(a, b):
+    """Multiply two numbers."""
+    return a * b
