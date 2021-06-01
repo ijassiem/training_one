aeecf40fae4e224d9cc791533d1ef2676562f3f9
ijassiem <ijassiem@ska.ac.za>
2021-06-01 14:25:22 +0200

    Added addition code

---
+++ addition.py
@@ -0,0 +1,11 @@
+"""
+This is an example docstring.
+
+A docstring with a summary line, a blank line
+and some more text like here.
+"""
+
+
+def square(a, b):
+    """Add two numbers."""
+    return a + b
