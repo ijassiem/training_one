a5acd92525c16072e2acae95cce109b5c41b855f
ijassiem <ijassiem@ska.ac.za>
2021-06-11 10:55:27 +0200

    Implement changes relating to comments in pull request
    These changes pertains to the comments indicated in the pull request: "Pokemon #3".
    
    Added numpy style docstrings to pokemon.py.
    
    Contributes to: DGDT-19

--- pokemon/pokemon.py
+++ pokemon/pokemon.py
@@ -5,26 +5,42 @@ class Pokemon(object):
     """
     This class Pokemon represents a pokemon character with several attributes.
 
-     :param name: Name of pokemon
-     :type name: str
-     :param nickname: Nickname of pokemon
-     :type nickname: str
-     :param moves: list of 4 moves
-     :type moves: list
+    Attributes
+    ----------
+    name : str
+        Name of pokemon.
+    nickname : str
+        Nickname of pokemon.
+    moves : list
+        A list of 4 pokemon moves.
     """
 
     def __init__(self, name, nickname):
-        """Initialise Pokemon class."""
+        """Initialise Pokemon class object with a name and nickname.
+
+        Parameters
+        ----------
+        name : str
+            Name of pokemon.
+        nickname : str
+            Nickname of pokemon.
+        """
         self.name = name
         self.nickname = nickname
         self.moves = ["jump", "strike", "dash", "block"]
 
     def speak(self):
-        """Pokemon say name."""
+        """Print the name of pokemon."""
         print("\n" + self.name.upper() + " " + self.name.upper() + "!")
 
     def learn_move(self, new_move):
-        """Pokemon learn a new move."""
+        """Add a new move to the list of the pokemon moves. The first item in list is removed.
+
+        Parameters
+        ----------
+        new_move : str
+            The name of the new move to be added to the list of moves.
+        """
         self.moves.append(new_move)
         self.moves.pop(0)
 
@@ -41,18 +57,30 @@ class ElectricPokemon(Pokemon):
     """
     This class ElectricPokemon inherits from the base class Pokemon, and represents a specific type pokemon character with several attributes.
 
-     :param name: Name of pokemon
-     :type name: str
-     :param nickname: Nickname of pokemon
-     :type nickname: str
-     :param moves: list of 4 moves
-     :type moves: list
-     :param pokemon_type: Type of pokemon
-     :type pokemon_type: str
+    Attributes
+    ----------
+    name : str
+        Name of pokemon.
+    nickname : str
+        Nickname of pokemon.
+    moves : list
+        A list of 4 pokemon moves.
+    pokemon_type : str
+        Type of pokemon.
     """
 
     def __init__(self, name, nickname, pokemon_type):
-        """Initialise ElectricPokemon class."""
+        """Initialise ELectricPokemon class object with a name, nickname and type.
+
+        Parameters
+        ----------
+        name : str
+            Name of pokemon.
+        nickname : str
+            Nickname of pokemon.
+        pokemon_type : str
+            Type of pokemon.
+        """
         self.pokemon_type = pokemon_type
         super(ElectricPokemon, self).__init__(name, nickname)
 
@@ -66,14 +94,14 @@ class ElectricPokemon(Pokemon):
         super(ElectricPokemon, self).print_details()
         print("\nSPECIES TYPE:", self.pokemon_type, "\n")
 
-
-p = Pokemon("pikachu", "pika")
-p.speak()
-p.print_details()
-print("")
-p.learn_move("glide")
-p.print_details()
-print("")
-e = ElectricPokemon("slowpoke", "poke", "psychic")
-e.speak()
-e.print_details()
+if __name__ == '__main__':
+    p = Pokemon("pikachu", "pika")
+    p.speak()
+    p.print_details()
+    print("")
+    p.learn_move("glide")
+    p.print_details()
+    print("")
+    e = ElectricPokemon("slowpoke", "poke", "psychic")
+    e.speak()
+    e.print_details()
